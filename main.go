66b51fe675026ff28945f9f2eaa9fa1e9c905f00
Andrey Dovban <dovban@andreyy.rambler.ru>
2024-11-07 01:09:30 +0300

    fix

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"github.com/ncruces/zenity"
+	"fmt"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-	zenity.Notify("There are system updates necessary!",
-		zenity.Title("Warning"),
-		zenity.InfoIcon,
-	)
+
+	cmd := exec.Command("cat")
+	r := strings.NewReader("dog1\ndog2")
+
+	cmd.Stdin = r
+	data, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(data))
+
 }
